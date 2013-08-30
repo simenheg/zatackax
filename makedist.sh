307c419bd646c80f4bdd3d24f4d595f41fb1226c
simenheg <simenheg@ifi.uio.no>
2013-08-30 12:20:51 +0200

    Added xz support to the makedist-script

--- makedist.sh
+++ makedist.sh
@@ -2,14 +2,15 @@
 
 if [ $1 ]; then
     make clean 1>/dev/null;
-    rel_name=zatackax-$1.src
+    rel_name=zatackax-$1.src;
     mkdir $rel_name;
     cp -r AUTHORS COPYING INSTALL NEWS README TODO data src \
         Makefile $rel_name;
-    tar czvpf $rel_name.tar.gz $rel_name
-    rm -rf $rel_name
-    echo "Release" $rel_name "ready!"
+    tar czvpf $rel_name.tar.gz $rel_name;
+    tar cJvpf $rel_name.tar.xz $rel_name;
+    rm -rf $rel_name;
+    echo "Release" $rel_name "ready!";
 else
-    echo "Specify a release name."
-    echo "  Example: ./makedist.sh 0.1.3"
+    echo "Specify a release name.";
+    echo "  Example: ./makedist.sh 0.1.3";
 fi
