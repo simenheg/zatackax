8173c6bd8369c02af70fe364f32cad13a71431a4
Simen Heggestøyl <simenheg@gmail.com>
2019-05-02 20:14:20 +0200

    Make 'make silent' functional
    Thanks to Sebastien Chevalier (ptitSeb).

--- src/common.h
+++ src/common.h
@@ -19,7 +19,9 @@
 #define O_VERBOSE   2   /* Important events (good for game logs, maybe?) */
 #define O_DEBUG     3   /* Debug mode */
 
-#ifdef VERBOSE
+#ifdef SILENT
+static const int olvl = O_SILENT;
+#elif VERBOSE
 static const int olvl = O_VERBOSE;
 #elif DEBUG
 static const int olvl = O_DEBUG;
