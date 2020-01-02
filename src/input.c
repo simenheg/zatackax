c241aace94643b8dad6cc00f02ea7bf0b8d44ab0
Simen Heggestøyl <simenheg@gmail.com>
2020-01-02 21:08:25 +0100

    Update copyright years

--- src/input.c
+++ src/input.c
@@ -1,5 +1,5 @@
 /* input -- keyboard, mouse and joystick/gamepad handling.
- * Copyright (C) 2010-2019 The Zatacka X development team
+ * Copyright (C) 2010-2020 The Zatacka X development team
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
